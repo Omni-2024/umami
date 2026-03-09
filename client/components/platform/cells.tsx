b25aa010bacdb3d1e0e167eb169cbe481a50c521
FalihAhamed <falih.20220943@iit.ac.lk>
2026-03-09 16:16:10 +0530

    cell animation added

--- client/components/platform/cells.tsx
+++ client/components/platform/cells.tsx
@@ -3,11 +3,47 @@ import Image from "next/image"
 
 const CellBackground: React.FC = () => {
   return (
-    <div className="absolute inset-0 overflow-hidden z-0">
+    <div className="absolute inset-0 overflow-hidden z-0 pointer-events-none">
+      {/* 1. BIOLOGICAL ANIMATION DEFINITIONS */}
+      <style>{`
+        @keyframes cellPulse {
+          0%, 100% { 
+            transform: scale(1) translate(0, 0) rotate(0deg);
+            opacity: 0.7;
+          }
+          33% { 
+            transform: scale(1.03) translate(15px, -10px) rotate(1deg);
+            opacity: 0.9;
+          }
+          66% { 
+            transform: scale(0.98) translate(-5px, 15px) rotate(-1deg);
+            opacity: 0.8;
+          }
+        }
 
-      {/* LEFT CELL */}
+        @keyframes cellDrift {
+          0%, 100% { 
+            transform: scale(1) translate(0, 0) rotate(0deg);
+            opacity: 0.8;
+          }
+          50% { 
+            transform: scale(1.05) translate(-20px, 20px) rotate(-2deg);
+            opacity: 1;
+          }
+        }
+
+        .biological-cell-left {
+          animation: cellPulse 18s ease-in-out infinite;
+        }
+
+        .biological-cell-right {
+          animation: cellDrift 25s ease-in-out infinite;
+        }
+      `}</style>
+
+      {/* LEFT CELL - High frequency "breathing" effect */}
       <div className="absolute lg:left-[2%] lg:top-[-0%] lg:w-[1020px] lg:h-[720px]
-      left-[-100%] top-[3%] w-[1020px] h-[620px] lg:opacity-100 opacity-50">
+        left-[-100%] top-[3%] w-[1020px] h-[620px] biological-cell-left">
         <Image
           src="/platform/cell1.png"
           alt="Marine Cell Left"
@@ -17,9 +53,9 @@ const CellBackground: React.FC = () => {
         />
       </div>
 
-      {/* RIGHT CELL */}
+      {/* RIGHT CELL - Heavy, slow drifting effect */}
       <div className="absolute lg:right-[0%] lg:top-[-7%] lg:w-[1220px] lg:h-[1020px]
-      right-[-40%] top-[-5%] w-[620px] h-[820px] lg:opacity-100 opacity-80">
+        right-[-40%] top-[-5%] w-[620px] h-[820px] biological-cell-right">
         <Image
           src="/platform/cell2.png"
           alt="Marine Cell Right"
@@ -29,8 +65,7 @@ const CellBackground: React.FC = () => {
         />
       </div>
 
-      {/* YELLOW GLOW */}
-      </div>
+    </div>
   )
 }
 
